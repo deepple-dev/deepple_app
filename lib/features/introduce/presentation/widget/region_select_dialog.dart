e65250c559f3d8eb7aa429ae9a71764c104f4132
genueB <bay9895@gmail.com>
2026-01-31 00:26:18 +0900

    null 처리

--- lib/features/introduce/presentation/widget/region_select_dialog.dart
+++ lib/features/introduce/presentation/widget/region_select_dialog.dart
@@ -11,7 +11,7 @@ class RegionSelectDialog extends StatefulWidget {
   State<RegionSelectDialog> createState() => _RegionSelectDialogState();
 
   static Future open(BuildContext context, List<String> selectedCityList) =>
-      showDialog<List<String>>(
+      showDialog<List<String>?>(
         context: context,
         builder: (context) =>
             RegionSelectDialog(selectedCityList: selectedCityList),
