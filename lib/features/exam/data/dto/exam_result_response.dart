1eee255f271e5c0f7bf1d1283b46503511dab0a3
goodmeet <jinjerkim@gmail.com>
2026-03-14 19:07:50 +0900

    feat: 가치관 테스트 dto 추가

---
+++ lib/features/exam/data/dto/exam_result_response.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:deepple_app/features/exam/domain/model/personality_type.dart';
+
+part 'exam_result_response.freezed.dart';
+part 'exam_result_response.g.dart';
+
+@freezed
+abstract class ExamResultResponse with _$ExamResultResponse {
+  const factory ExamResultResponse({required PersonalityType personalityType}) =
+      _ExamResultResponse;
+
+  factory ExamResultResponse.fromJson(Map<String, dynamic> json) =>
+      _$ExamResultResponseFromJson(json);
+}
